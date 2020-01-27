1e6ce481a4905b0e79d806d493d3aae6c53334d8
thanawatgulati <thanawatgulatijames@gmail.com>
2020-01-27 21:16:54 +0700

    🎵 start learn gatsbyJS

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <div>
+    <h1>About ME</h1>
+    <p>This is my personal website</p>
+    <Link to="/">&larr; Back to home</Link>
+  </div>
+);
